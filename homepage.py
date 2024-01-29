9740b1f6770c0a2e4992f228965666d58350d1cc
subedianukaran <anukaransubedi@gmail.com>
2024-01-29 18:24:05 +0545

    working foreign key

--- homepage.py
+++ homepage.py
@@ -4,8 +4,9 @@ import sqlite3
 from tkinter import simpledialog
 from datetime import datetime
 
+
 class AttendanceManager:
-    def __init__(self,conn,class_name):
+    def __init__(self, conn, class_name):
 
         self.class_name = class_name
         self.conn = conn
@@ -13,145 +14,99 @@ class AttendanceManager:
 
         self.take_attendance()
 
-
     def take_attendance(self):
-
-        date = datetime.today().date()
-
-        if date:
-            column_check_query = f"PRAGMA table_info({self.class_name}_attendance)"
-            self.cursor.execute(column_check_query)
-            columns = [column[1] for column in self.cursor.fetchall()]
-
-            if date in columns:
-                messagebox.showinfo("Info", "Already taken, go to edit data to make changes.")
-            else:
-
-                self.cursor.execute(f"ALTER TABLE {self.class_name}_attendance ADD COLUMN '{date}' BIT")
-                self.conn.commit()
-
-                student_query = f"SELECT Name, RollNo FROM {self.class_name}_names ORDER BY Name"
-                self.cursor.execute(student_query)
-                self.students = self.cursor.fetchall()
-
-                self.attendance_window = tk.Toplevel()
-                self.attendance_window.title(f"Attendance for {date}")
-
-                self.current_student = 0
-                self.show_attendance(self.students[self.current_student], date)
-
+        pass
 
     def show_attendance(self, student, date):
-        name_label = tk.Label(self.attendance_window, text=f"Name of Student: {student[0]}")
-        name_label.pack()
-
-        roll_label = tk.Label(self.attendance_window, text=f"Roll No: {student[1]}")
-        roll_label.pack()
-
-        present_button = tk.Button(self.attendance_window, text="Present", command=lambda: self.mark_attendance(date, student[1], status = True))
-        present_button.pack()
-
-        absent_button = tk.Button(self.attendance_window, text="Absent", command=lambda: self.mark_attendance(date, student[1], status = False))
-        absent_button.pack()
-
+        pass
 
     def mark_attendance(self, date, roll_no, status):
-        update_query = f"UPDATE {self.class_name}_attendance SET '{date}' = ? WHERE RollNo = ?"
-        self.cursor.execute(update_query, (status, roll_no))
-        self.conn.commit()
-
-        self.current_student += 1
-
-        if self.current_student < len(self.students):
-            self.show_attendance(self.students[self.current_student], date)
-        else:
-            messagebox.showinfo("Info", "Attendance taken for all students.")
-            self.attendance_window.destroy()
+        pass
 
 
 class ClassHomePage:
-    def __init__(self, class_name, root,cursor, conn):
+    def __init__(self, class_name, root, cursor, conn):
         self.class_name = class_name
         self.root = root
         self.cursor = cursor
         self.conn = conn
-        pass
 
     def take_attendance(self):
-        attendance = AttendanceManager(self.conn, self.class_name)
-        attendance.take_attendance()
-
+        pass
 
     def edit_records(self):
         pass
 
-
     def view_statistics(self):
         pass
-
 
     def delete_records(self):
         pass
 
-
     def view_students(self):
         pass
-
 
     def remove_students(self):
         def remove_students():
             student_names = student_entry.get()
             student_window.destroy()
-            student_list = [name.strip() for name in student_names.split(',')]
+            student_list = [name.strip() for name in student_names.split(",")]
             students_not_found = []
 
             try:
 
                 for name in student_list:
                     if name:
-                        self.cursor.execute(f"DELETE FROM {self.class_name}_names WHERE Name = ?", (name,))
+                        self.cursor.execute(
+                            f"DELETE FROM students WHERE student_name = ?", (name,)
+                        )
                         if self.cursor.rowcount == 0:
                             students_not_found.append(name)
 
                 self.conn.commit()
 
                 if students_not_found:
-                    messagebox.showwarning("Students Not Found",
-                                           f"The following students weren't found: {', '.join(students_not_found)}")
+                    messagebox.showwarning(
+                        "Students Not Found",
+                        f"The following students weren't found: {', '.join(students_not_found)}",
+                    )
                 else:
                     messagebox.showinfo("Success", "Students removed successfully.")
 
             except sqlite3.Error as e:
                 messagebox.showerror("Database Error", f"Error: {e}")
 
-
         student_window = tk.Toplevel()
         student_window.title("Remove Students")
 
-        student_label = tk.Label(student_window, text="Enter student names to remove (separated by comma):")
+        student_label = tk.Label(
+            student_window, text="Enter student names to remove (separated by comma):"
+        )
         student_label.pack()
 
         student_entry = tk.Entry(student_window, width=40)
         student_entry.pack()
 
-        remove_button = tk.Button(student_window, text="Remove", command=remove_students)
+        remove_button = tk.Button(
+            student_window, text="Remove", command=remove_students
+        )
         remove_button.pack()
-
 
     def edit_students(self):
         pass
-
 
     def add_students(self):
         def add_students_d():
             student_names = student_entry.get()
             student_window.destroy()
-            student_list = [name.strip() for name in student_names.split(',')]
+            student_list = [name.strip() for name in student_names.split(",")]
 
             try:
                 for name in student_list:
                     if name:
-                        self.cursor.execute(f"INSERT INTO {self.class_name}_names (Name) VALUES (?)", (name,))
+                        self.cursor.execute(
+                            f"INSERT INTO students(student_name) VALUES (?)", (name,)
+                        )
                         self.conn.commit()
 
                 messagebox.showinfo("Success", "Students added successfully.")
@@ -162,7 +117,9 @@ class ClassHomePage:
         student_window = tk.Toplevel()
         student_window.title("Add Students")
 
-        student_label = tk.Label(student_window, text="Enter student names (separated by comma):")
+        student_label = tk.Label(
+            student_window, text="Enter student names (separated by comma):"
+        )
         student_label.pack()
 
         student_entry = tk.Entry(student_window, width=40)
@@ -171,19 +128,34 @@ class ClassHomePage:
         add_button = tk.Button(student_window, text="Add", command=add_students_d)
         add_button.pack()
 
-
     def classpage(self):
         self.class_frame = tk.Frame(self.root)
         self.class_frame.pack(fill=tk.BOTH, expand=True)
 
-        class_label = tk.Label(self.class_frame, text=self.class_name, font=("Arial", 18, "bold"))
+        class_label = tk.Label(
+            self.class_frame, text=self.class_name, font=("Arial", 18, "bold")
+        )
         class_label.grid(pady=30)
         class_label.pack()
 
-        button_texts = ["Take Attendance", "Edit Records", "View Statistics",
-                        "Delete Records", "View Students","Add Students", "Remove Students", "Edit Student Data"]
-        button_commands = [self.take_attendance, self.edit_records, self.view_statistics, self.delete_records,
-                           self.view_students, self.add_students, self.remove_students, self.edit_students]
+        button_texts = [
+            "Take Attendance",
+            "Edit Records",
+            "View Statistics",
+            "Delete Records",
+            "View Students",
+            "Add Students",
+            "Remove Students",
+        ]
+        button_commands = [
+            self.take_attendance,
+            self.edit_records,
+            self.view_statistics,
+            self.delete_records,
+            self.view_students,
+            self.add_students,
+            self.remove_students,
+        ]
 
         for i in range(0, len(button_texts), 2):
             frame = tk.Frame(self.class_frame)
@@ -191,113 +163,117 @@ class ClassHomePage:
 
             for j in range(2):
                 if i + j < len(button_texts):
-                    button = tk.Button(frame, text=button_texts[i + j], width=15, command=button_commands[i+j])
+                    button = tk.Button(
+                        frame,
+                        text=button_texts[i + j],
+                        width=15,
+                        command=button_commands[i + j],
+                    )
                     button.pack(side=tk.LEFT, padx=20, pady=20)
 
 
 class MainPage:
-    def __init__(self,database):
+    def __init__(self, username):
         self.root = tk.Tk()
         self.root.title("Attendance Application")
         self.root.geometry("600x450")
-        self.database = database
-        self.conn = sqlite3.connect(f"{self.database}.db")
+        self.conn = sqlite3.connect("Database.db")
         self.cursor = self.conn.cursor()
-
+        self.username = username
+        self.cursor.execute("")
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS classes (class_id INTEGER PRIMARY KEY AUTOINCREMENT, class_name VARCHAR(255));"
+        )
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS students (student_id INTEGER PRIMARY KEY AUTOINCREMENT, student_name VARCHAR(255));"
+        )
+        self.conn.commit()
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS user_class (user_id INTEGER, class_id INTEGER, PRIMARY KEY (user_id, class_id), FOREIGN KEY (user_id) REFERENCES users(user_id), FOREIGN KEY (class_id) REFERENCES classes(class_id));"
+        )
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS class_student (class_id INTEGER, student_id INTEGER, PRIMARY KEY (class_id, student_id), FOREIGN KEY (class_id) REFERENCES classes(class_id), FOREIGN KEY (student_id) REFERENCES students(student_id));"
+        )
+        self.conn.commit()
         self.home_page()
 
-
     def add_new_class(self):
-        new_class_name = tk.simpledialog.askstring("Add New Class", "Enter new class name:")
+        self.new_class_name = tk.simpledialog.askstring(
+            "Add New Class", "Enter new class name:"
+        )
 
-        if new_class_name:
-            class_exists = self.check_class_exists(new_class_name)
-
-            if class_exists:
+        if self.new_class_name:
+            self.cursor.execute(
+                "SELECT 1 FROM classes WHERE class_name = ? LIMIT 1",
+                (self.new_class_name,),
+            )
+            self.conn.commit()
+            existing_class = self.cursor.fetchone()
+            if existing_class:
                 messagebox.showinfo("Class Exists", "Class already exists.")
             else:
-                self.cursor.execute('INSERT INTO RecordList (Class) VALUES (?)', (new_class_name,))
+                self.cursor.execute(
+                    "INSERT INTO classes (class_name) VALUES (?)",
+                    (self.new_class_name,),
+                )
                 self.conn.commit()
-                self.create_class_tables(new_class_name)
-                messagebox.showinfo("Success", f"Class '{new_class_name}' added successfully.")
+                self.cursor.execute(
+                    """INSERT INTO user_class (user_id, class_id)
+    VALUES ((SELECT user_id FROM users WHERE username = ?), (SELECT class_id FROM classes WHERE class_name = ?));
+""",
+                    (self.username, self.new_class_name),
+                )
+                self.conn.commit()
+                messagebox.showinfo(
+                    "Success", f"Class '{self.new_class_name}' added successfully."
+                )
                 self.home_frame.destroy()
                 self.home_page()
 
-
-    def create_class_tables(self, class_name):
-        create_names_table_query = f'''
-            CREATE TABLE IF NOT EXISTS {class_name}_names (
-                RollNo INTEGER PRIMARY KEY,
-                Name TEXT
-            )
-        '''
-        self.cursor.execute(create_names_table_query)
-        self.conn.commit()
-
-        create_attendance_table_query = f'''
-            CREATE TABLE IF NOT EXISTS {class_name}_attendance (
-                RollNo INTEGER,
-                FOREIGN KEY(RollNo) REFERENCES {class_name}_names (RollNo)
-            )
-        '''
-        self.cursor.execute(create_attendance_table_query)
-        self.conn.commit()
-
-
     def display_classes(self):
-        self.cursor.execute('SELECT Class FROM RecordList')
+        self.cursor.execute("SELECT class_name FROM classes")
         classes = self.cursor.fetchall()
 
         for class_name in classes:
-            tk.Button(self.home_frame, text=class_name[0], command=lambda cn=class_name[0].title(): self.class_details(cn),
-                      width=10, height=2).pack(padx=5, pady=5)
-
+            tk.Button(
+                self.home_frame,
+                text=class_name[0],
+                command=lambda cn=class_name[0].title(): self.class_details(cn),
+                width=10,
+                height=2,
+            ).pack(padx=5, pady=5)
 
     def remove_class(self):
-        index = tk.simpledialog.askinteger("Remove Class", "Enter index of the class to remove:")  # Get index from user
-
-        if index is not None:
-            classes = self.cursor.execute('SELECT Class FROM RecordList').fetchall()
-
-            if 0 <= index < len(classes):
-                class_name = classes[index][0]
-                self.cursor.execute(f"DELETE FROM RecordList WHERE Class='{class_name}'")
-                self.cursor.execute(f'DROP TABLE {class_name}_names')
-                self.cursor.execute(f'DROP TABLE {class_name}_attendance')
-                self.conn.commit()
-                messagebox.showinfo("Success", f"Class '{class_name}' removed successfully.")
-                self.home_frame.destroy()
-                self.home_page()
-
-            else:
-                messagebox.showwarning("Invalid Index", "Please enter a valid index.")
-
+        pass
 
     def check_class_exists(self, class_name):
-        self.cursor.execute('SELECT * FROM RecordList WHERE Class=?', (class_name,))
-        return self.cursor.fetchone()
-
+        pass
 
     def class_details(self, class_name):
         self.home_frame.destroy()
-        class_page = ClassHomePage(class_name, self.root,self.cursor,self.conn)
+        class_page = ClassHomePage(class_name, self.root, self.cursor, self.conn)
         class_page.classpage()
-
 
     def home_page(self):
         self.home_frame = tk.Frame(self.root)
         self.home_frame.pack(fill=tk.BOTH, expand=True)
 
-        label_classes = tk.Label(self.home_frame, text="Classes", font=("Arial", 24, "bold"))
+        label_classes = tk.Label(
+            self.home_frame, text="Classes", font=("Arial", 24, "bold")
+        )
         label_classes.pack(pady=20)
 
-        self.display_classes() 
+        self.display_classes()
 
         button_frame = tk.Frame(self.home_frame)
         button_frame.pack(side=tk.BOTTOM, fill=tk.X, pady=10)
 
-        self.add_class_button = tk.Button(button_frame, text="Add New Class", command=self.add_new_class)
+        self.add_class_button = tk.Button(
+            button_frame, text="Add New Class", command=self.add_new_class
+        )
         self.add_class_button.pack(side=tk.LEFT, padx=10)
 
-        self.remove_class_button = tk.Button(button_frame, text="Remove Class", command=self.remove_class)
+        self.remove_class_button = tk.Button(
+            button_frame, text="Remove Class", command=self.remove_class
+        )
         self.remove_class_button.pack(side=tk.RIGHT, padx=10)
--- login.py
+++ login.py
@@ -3,6 +3,8 @@ from tkinter import messagebox
 import sqlite3
 import hashlib
 from homepage import MainPage
+from homepage import ClassHomePage
+from homepage import AttendanceManager
 
 
 class DatabaseManager:
@@ -36,39 +38,43 @@ class Login:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Login")
-        self.root.geometry("450x250") # 450 x 250
+        self.root.geometry("450x250")  # 450 x 250
 
-        self.database = DatabaseManager('Database.db')
+        self.database = DatabaseManager("Database.db")
         self.create_user_table()
 
         self.create_login_page()
 
-    def encrypt_password(self,password):
+    def encrypt_password(self, password):
         sha256 = hashlib.sha256()
 
-        sha256.update(password.encode('utf-8'))
+        sha256.update(password.encode("utf-8"))
 
         encrypted_password = sha256.hexdigest()
 
         return encrypted_password
 
     def create_user_table(self):
-        columns = "user_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL"
-        self.database.create_table('users', columns)
-
+        columns = "user_id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(255), password VARCHAR(255)"
+        self.database.create_table("users", columns)
 
     def submit(self):
         new_username = self.entry_new_username.get()
         new_password = self.entry_new_password.get()
         enc_password = self.encrypt_password(new_password)
 
-        query = 'SELECT * FROM Users WHERE username=?'
+        query = "SELECT * FROM Users WHERE username=?"
         existing_user = self.database.fetch_all(query, (new_username,))
 
         if existing_user:
-            messagebox.showerror("Username Exists", "Username already exists. Try logging in instead.")
+            messagebox.showerror(
+                "Username Exists", "Username already exists. Try logging in instead."
+            )
         else:
-            self.database.execute_query('INSERT INTO users (username, password) VALUES (?, ?)', (new_username, enc_password))
+            self.database.execute_query(
+                "INSERT INTO users (username, password) VALUES (?, ?)",
+                (new_username, enc_password),
+            )
             messagebox.showinfo("Success", "User Added")
 
     def login_conn(self):
@@ -76,12 +82,12 @@ class Login:
         entered_password = self.entry_password.get()
         enc_password = self.encrypt_password(entered_password)
 
-        query = 'SELECT * FROM users WHERE username=? AND password=?'
+        query = "SELECT * FROM users WHERE username=? AND password=?"
         user = self.database.fetch_all(query, (entered_username, enc_password))
         if user:
             messagebox.showinfo("Login", f"Welcome, {entered_username}!")
             self.root.destroy()
-            homepage= MainPage(entered_username)
+            homepage = MainPage(entered_username)
             homepage.root.mainloop()
         else:
             messagebox.showerror("Login Error", "Credentials don't match")
@@ -89,16 +95,18 @@ class Login:
     def remove_user(self):
         selected_user_id = int(self.entry_index.get())
 
-        conn = sqlite3.connect('Database.db')
+        conn = sqlite3.connect("Database.db")
         cursor = conn.cursor()
 
-        cursor.execute('SELECT * FROM users')
+        cursor.execute("SELECT * FROM users")
         users = cursor.fetchall()
 
         if any(user[0] == selected_user_id for user in users):
-            cursor.execute('DELETE FROM users WHERE user_id=?', (selected_user_id,))
+            cursor.execute("DELETE FROM users WHERE user_id=?", (selected_user_id,))
             conn.commit()
-            messagebox.showinfo("Success", f"User with user_id {selected_user_id} removed successfully.")
+            messagebox.showinfo(
+                "Success", f"User with user_id {selected_user_id} removed successfully."
+            )
             self.remove_frame.destroy()
             self.create_remove_page()
         else:
@@ -106,11 +114,10 @@ class Login:
 
         conn.close()
 
-
     def create_login_page(self):
-        if hasattr(self, 'remove_frame'):
+        if hasattr(self, "remove_frame"):
             self.remove_frame.pack_forget()
-        if hasattr(self, 'signup_frame'):
+        if hasattr(self, "signup_frame"):
             self.signup_frame.pack_forget()
         self.login_frame = tk.Frame(self.root)
         self.login_frame.pack(fill=tk.BOTH, expand=True)
@@ -124,23 +131,31 @@ class Login:
         self.label_password = tk.Label(self.login_frame, text="Password:")
         self.label_password.pack()
 
-        self.entry_password = tk.Entry(self.login_frame, show='*')
+        self.entry_password = tk.Entry(self.login_frame, show="*")
         self.entry_password.pack()
 
-        button_login = tk.Button(self.login_frame, text="Login", command=lambda: self.login_conn())
+        button_login = tk.Button(
+            self.login_frame, text="Login", command=lambda: self.login_conn()
+        )
         button_login.pack()
 
         label_new_user = tk.Label(self.login_frame, text="New User?")
         label_new_user.pack(side=tk.LEFT)
 
-        button_signup = tk.Button(self.login_frame, text="Sign Up", command=lambda: self.create_signup_page())
+        button_signup = tk.Button(
+            self.login_frame, text="Sign Up", command=lambda: self.create_signup_page()
+        )
         button_signup.pack(side=tk.LEFT)
 
-        button_remove_user = tk.Button(self.login_frame, text="Remove Existing User", command=lambda: self.create_remove_page())
+        button_remove_user = tk.Button(
+            self.login_frame,
+            text="Remove Existing User",
+            command=lambda: self.create_remove_page(),
+        )
         button_remove_user.pack(side=tk.RIGHT)
 
     def create_signup_page(self):
-        if hasattr(self, 'signup_frame'):
+        if hasattr(self, "signup_frame"):
             self.signup_frame.pack_forget()
 
         self.login_frame.pack_forget()
@@ -157,17 +172,23 @@ class Login:
         self.label_new_password = tk.Label(self.signup_frame, text="New Password:")
         self.label_new_password.pack()
 
-        self.entry_new_password = tk.Entry(self.signup_frame, show='*')
+        self.entry_new_password = tk.Entry(self.signup_frame, show="*")
         self.entry_new_password.pack()
 
-        button_submit = tk.Button(self.signup_frame, text="Submit", command=lambda: self.submit())
+        button_submit = tk.Button(
+            self.signup_frame, text="Submit", command=lambda: self.submit()
+        )
         button_submit.pack()
 
-        button_return_login = tk.Button(self.signup_frame, text="Return to Login Page", command=lambda: self.create_login_page())
+        button_return_login = tk.Button(
+            self.signup_frame,
+            text="Return to Login Page",
+            command=lambda: self.create_login_page(),
+        )
         button_return_login.pack()
 
     def create_remove_page(self):
-        if hasattr(self, 'remove_frame'):
+        if hasattr(self, "remove_frame"):
             self.remove_frame.pack_forget()
 
         self.login_frame.pack_forget()
@@ -175,12 +196,14 @@ class Login:
         self.remove_frame = tk.Frame(self.root)
         self.remove_frame.pack(fill=tk.BOTH, expand=True)
 
-        label_instructions = tk.Label(self.remove_frame, text="Enter user_id of user to remove:")
+        label_instructions = tk.Label(
+            self.remove_frame, text="Enter user_id of user to remove:"
+        )
         label_instructions.pack()
 
-        conn = sqlite3.connect('Database.db')
+        conn = sqlite3.connect("Database.db")
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM users')
+        cursor.execute("SELECT * FROM users")
         users = cursor.fetchall()
 
         for user in users:
@@ -190,14 +213,19 @@ class Login:
         self.entry_index = tk.Entry(self.remove_frame)
         self.entry_index.pack()
 
-        button_remove = tk.Button(self.remove_frame, text="Remove", command=lambda: self.remove_user())
+        button_remove = tk.Button(
+            self.remove_frame, text="Remove", command=lambda: self.remove_user()
+        )
         button_remove.pack()
 
-        button_return_login = tk.Button(self.remove_frame, text="Return to Login Page", command=lambda: self.create_login_page())
+        button_return_login = tk.Button(
+            self.remove_frame,
+            text="Return to Login Page",
+            command=lambda: self.create_login_page(),
+        )
         button_return_login.pack()
 
         conn.close()
-
 
 
 display_instance = Login()
